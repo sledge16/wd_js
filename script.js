e60a78e05fb52c8c173c522e52a51bd9ee18b2db
loz <sledgelozon@gmail.com>
2022-06-28 10:23:21 +0200

    update

--- script.js
+++ script.js
@@ -15,22 +15,29 @@ b = 50 // PAREIL
 c = '80' // PAREIL
 b = 90 // B va contenir 90
 
-console.log(a + parseInt(c));
-console.error(typeof(c));
-console.error(b);
+a = 10
+b = 70
+c = "salut"
+c = 5
+console.error("a+b");
+console.error(c)
+SEL = 100
+SUCRE = 80
+console.error("Nous avons du SEL Q. de " + SEL + ". Nous avons du sucre de Q. de " + SUCRE)
+TEMP = SEL;
+SEL = SUCRE;
+SUCRE = TEMP;
+console.error("Nous avons du SEL Q. de " + SEL + ". Nous avons du sucre de Q. de " + SUCRE)
+    // age = prompt("quel est ton age ?");
+    // B=parstInt(age)+2
+    // console.error("L'utilisateur à"+ age +"ans");
+    // console.error("L'utilisateur à"+ B +"ans");
 
-// EXO : LES VARIABLES
-SEL = 100;
-SURCRE = 80;
-console.error("Nous avons du SEL en Q.de" + SEL + ". Nous avons du sucre en quantite" + sucre);
+prix = prompt("quel est le prix de l'article?")
+console.error("l'article vaut " + prix + "euros")
+TTC = parseInt(prix) * 1.20
+console.error("l'article vaut " + TTC + "euros TTC")
 
-// VOTRE CODE INVERSE LES VARIABLE
-
-//CREEZ UN USTENSIL / VARIABLE QUI SERT DE MEMOIRE
-
-SURCRE = 100;
-SEL = 80;
-console.log("SURCRE,SEL")
-
-
-console.error("Nous avons du SEL en Q.de" + SEL + "Nous avons du sucre en quantite" + sucre);
+date = Date()
+date = new Date()
+console.error("aujourdhuit nous somme le" + date)
--- script2.js
+++ script2.js
@@ -1,15 +1,29 @@
-age = prompt("Veuille entre votre age?");
-
+age = prompt("Veuiller entré votre âge")
 age = parseInt(age)
-
 if (age > 18) {
-    console.error("Vous etes majeur");
+    console.error("vous êtes majeur")
 }
-
 if (age < 18) {
-    console.error("Vous etes piler minuer");
+    console.error("vous êtes mineur")
+
+}
+if (age == 18) {
+    console.error("vous pille majeur")
 }
 
-if (age === 18) {
-    console.error("Vous etes mineur");
+nombre = prompt("veuillez entré un nombre")
+nombre = parseInt(nombre)
+if (nombre > 0) {
+    console.error("le nombre est positif")
+}
+if (nombre < 0) {
+    console.error("le nombre est négatif")
+}
+
+number = prompt("Veuiller entré un nombre")
+number = parseInt(number)
+if ((number % 2) == 0) {
+    console.error("le nombre est paire")
+} else {
+    console.error("le nombre est impaire")
 }
